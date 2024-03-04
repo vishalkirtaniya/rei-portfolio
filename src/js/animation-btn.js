72fd18deed8ae9f5eaf314a14edee8380d321554
Vishal Aditya Kirtaniya <vishalkirtaniyaofficial@gmail.com>
2024-03-04 22:14:48 +0530

    changed logo of the sound design button

--- src/js/animation-btn.js
+++ src/js/animation-btn.js
@@ -5,17 +5,17 @@ const audio = document.getElementById("my-audio");
 let isPlaying = false;
 
 changeLogoBtn.addEventListener("click", function () {
-  if (logo.src.includes("music-note.svg")) {
+  if (logo.src.includes("play.svg")) {
     logo.style.transform = "rotate(360deg) scale(1.5)"; // Example transition effect
     setTimeout(() => {
-      logo.src = "./src/img-folder/icons-folder/color-palette.svg"; // Change to the new logo path
+      logo.src = "./src/img-folder/icons-folder/pause.svg"; // Change to the new logo path
       logo.style.transform = "rotate(0deg) scale(1)"; // Reset transform
       playMusic();
     }, 500); // Adjust timing to match transition duration
   } else {
     logo.style.transform = "rotate(360deg) scale(1.5)"; // Example transition effect
     setTimeout(() => {
-      logo.src = "./src/img-folder/icons-folder/music-note.svg"; // Restore original logo
+      logo.src = "./src/img-folder/icons-folder/play.svg"; // Restore original logo
       logo.style.transform = "rotate(0deg) scale(1)"; // Reset transform
       pauseMusic();
     }, 500); // Adjust timing to match transition duration
