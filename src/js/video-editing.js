d51c82b631fb03db1a15e2316f2f9530adc24a35
Vishal Aditya Kirtaniya <vishalkirtaniyaofficial@gmail.com>
2024-03-05 17:50:40 +0530

    button -> video editing, added animations in the button

---
+++ src/js/video-editing.js
@@ -0,0 +1,6 @@
+const video2 = document.getElementById("video-logo");
+const playButton2 = document.getElementById("videobutton");
+
+playButton2.addEventListener("click", function () {
+  video2.play();
+});
